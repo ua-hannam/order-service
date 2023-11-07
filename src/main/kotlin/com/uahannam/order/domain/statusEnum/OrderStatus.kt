c41739b27097dbf99bdcb8ae0349152f9f4e9912
서민재 <seominjae@seominjaeui-MacBookPro.local>
2023-11-07 17:48:58 +0900

    ⚙️ chore: 주문 상태 추가 - 사용자 취소 완료

--- src/main/kotlin/com/uahannam/order/domain/statusEnum/OrderStatus.kt
+++ src/main/kotlin/com/uahannam/order/domain/statusEnum/OrderStatus.kt
@@ -7,6 +7,7 @@ enum class OrderStatus(
 
     RECEIPT("RECEIPT", "주문 접수"),
     USER_CANCEL_REQUEST("USER_CANCEL_REQUEST", "사용자 취소 요청"),
+    USER_CANCEL("USER_CANCEL", "사용자 취소"),
     STORE_CANCEL("STORE_CANCEL", "가게 사정 취소"),
     APPROVAL("APPROVAL", "주문 승인")
 }
