50b7bf135671dc80e49171982b47bcbc8367e48a
DevSeoRex <ckdekrn88@gmail.com>
2023-11-23 22:19:16 +0900

    🚀 CI: CI Test 중 #2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.0.12"
 	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"  
 } 
 
 group = "com.uahannam"
