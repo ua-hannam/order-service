93e6f63c619868bb5d6f0b2f30cc6f49dea8113d
DevSeoRex <ckdekrn88@gmail.com>
2023-12-02 19:28:20 +0900

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -19,7 +19,7 @@ node {
     }
 
     stage('Push image') {
-        docker.withRegistry('http://harbor.uahannam.io', 'harbor') {
+        docker.withRegistry('https://192.168.45.205', 'harbor') {
             // app.push("${env.BUILD_NUMBER}")
             app.push("latest")
         }
