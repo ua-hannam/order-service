20f65cfa22d7f52e18af84599d2a5bd70f44450d
Sungmin Kim <83997823+keeptrynjoy@users.noreply.github.com>
2023-12-17 17:02:28 +0900

    🚀 CI: Slack 알림 기능 추가

--- jenkinsfile-dev.groovy
+++ jenkinsfile-dev.groovy
@@ -1,16 +1,33 @@
 def app
+def massage = "_${env.JOB_NAME}_[#${env.BUILD_NUMBER}] <${env.BUILD_URL}|OPEN>"
 
 node {
+    try {
+    //Slack send notify - start
+    slackSend(channel: '#backend-bulid-log', message: "*Build start(${massage})*")
+        
     stage('Checkout') {
         checkout scm 
     }
 
     stage('Ready') {      
-        echo 'Ready to build'
-        gradleHome = tool 'gradle' 
+        echo 'Ready to build' 
+        gradleHome = tool 'gradle'   
     }
 
     stage('Build') {
-        sh "${gradleHome}/bin/gradle clean build"
+        sh "${gradleHome}/bin/gradle clean build -x test"
+    }
+
+    //Slack send notify - result
+    slackSend(channel: '#backend-bulid-log', color: '#00FF00', message: """
+*Build successful*
+Job : ${massage}
+""")
+    } catch (Exception e) {
+        slackSend(channel: '#backend-bulid-log', color: 'danger', message: """ 
+*Build failed*
+Job : ${massage}
+""")
     }
 }
